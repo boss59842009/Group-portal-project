8ad6180043e738a8668a3f2dd856510cf2b1fef8
Dmitro411 <dk0734303536@gmail.com>
2025-11-26 19:49:36 +0200

    Add models

---
+++ events_calendar/forms.py
@@ -0,0 +1,17 @@
+# class EventsFilterForm(Form):
+#     FILTER_MONTH = [
+#         ("January", "Січень"),
+#         ("February", "Лютий"),
+#         ("March", "Березень"),
+#         ("April", "Квітень"),
+#         ("May", "Травень"),
+#         ("June", "Червень"),
+#         ("July", "Липень"),
+#         ("Augest", "Серпень"),
+#         ("September", "Вересень"),
+#         ("October", "Жовтень"),
+#         ("November", "Листопад"),
+#         ("December", "Грудень"),
+#     ]
+
+# month = ChoiceField(choices = FILTER_MONTH, )
--- events_calendar/views.py
+++ events_calendar/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from events_calendar import models, forms
+
+class EventsListView(ListView):
+    model = models.Event
+    context_object_name = 'events'
+    template_name = 'tasks/...html'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['filter_form'] = forms.EventsFilterForm(self.request.GET)
+    #     return context
+
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+        
+
+
+class EventsDetailView(DetailView):
+    pass
+
+class EventsCreatelView(CreateView):
+    pass
+
+class EventsUpdateView(UpdateView):
+    pass
+
+class EventsDeleteView(DeleteView):
+    pass

622df3b6452127213102884de0b90628adba3e3b
Yevhenii Semeniuk <boss59842009@gmail.com>
2025-12-03 17:35:37 +0200

    add urls and view templates to advertisements

---
+++ advertisements/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from advertisements import views
+from advertisements.views import AdvertisementDeleteView, AdvertisementDetailView, AdvertisementUpdateView
+
+urlpatterns = [
+    path('advertisements/', AdvertisementDeleteView.as_view(), name='advertisements-list'),
+    path("advertisement/<int:pk>/", AdvertisementDetailView.as_view(), name='advertisement-detail'),
+    path("advertisement/create/", AdvertisementDetailView.as_view(), name='advertisement-create'),
+    path("advertisement/update/<int:pk>/", AdvertisementUpdateView.as_view(), name='advertisement-update'),
+    path("advertisement/delete/<int:pk>/", AdvertisementDeleteView.as_view(), name='advertisement-delete'),
+]
--- advertisements/views.py
+++ advertisements/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+
+class AdvertisementListView(ListView):
+    pass
+
+class AdvertisementDetailView(DetailView):
+    pass
+
+class AdvertisementCreateView(CreateView):
+    pass
+
+class AdvertisementUpdateView(UpdateView):
+    pass
+
+class AdvertisementDeleteView(DeleteView):
+    pass

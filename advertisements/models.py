d6817862a019130a8719f27e5857af7a3af9d6fa
Yevhenii Semeniuk <boss59842009@gmail.com>
2025-12-24 17:12:17 +0200

    small bugs fixed

--- advertisements/models.py
+++ advertisements/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Advertisement'
+        verbose_name_plural = 'Advertisements'
